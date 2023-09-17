b253db55f159ed6d4cd9dd12db396eee55b8c6b5
Xinyu Meng <xmeng975@gmail.com>
2023-09-17 13:26:31 -0400

    tokenizer

---
+++ lab1/lab1_assign/lab1.cpp
@@ -0,0 +1,85 @@
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <string>
+#include <vector>
+#include <map>
+#include <regex>
+
+using namespace std;
+
+struct Symbol
+{
+    string name;
+    int module;
+    int address;
+};
+
+struct Instruction
+{
+    char addrMode;
+    int operand;
+};
+
+struct Module
+{
+    int baseAddress;
+    vector<Symbol> definitions;
+    vector<string> useList;
+    vector<Instruction> instructions;
+};
+
+map<string, Symbol> symbolTable;
+vector<Module> moduleBaseTable;
+int currentModule = 0;
+
+void passOne(ifstream &inputFile)
+{
+    // Implement pass one logic here
+}
+
+void passTwo(ifstream &inputFile)
+{
+    // Implement pass two logic here
+}
+
+vector<string> getToken(string filename)
+{
+    vector<string> tokens;
+    ifstream inputFile(filename);
+    if (!inputFile.is_open())
+    {
+        std::cerr << "Error: Unable to open file " << filename << std::endl;
+        return tokens;
+    }
+    string line;
+    while (getline(inputFile, line))
+    {
+        regex delimiter("[ \t\n]+");
+        sregex_token_iterator it(line.begin(), line.end(), delimiter, -1);
+        sregex_token_iterator end;
+        while (it != end)
+        {
+            tokens.push_back(it->str());
+            ++it;
+        }
+    }
+    inputFile.close();
+    return tokens;
+}
+
+int main(int argc, char *argv[])
+{
+    if (argc != 2)
+    {
+        cerr << "Usage: " << argv[0] << " <input_file>" << endl;
+        return 1;
+    }
+
+    vector<string> tokens = getToken(argv[1]);
+    for (const std::string &token : tokens)
+    {
+        cout << token << endl;
+    }
+    return 0;
+}
