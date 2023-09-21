a2f4366332b4c3128f1aafb42870dfc51a73dc7a
Xinyu Meng <xmeng975@gmail.com>
2023-09-20 21:08:48 -0400

    pass1

--- lab1/lab1_assign/lab1.cpp
+++ lab1/lab1_assign/lab1.cpp
@@ -35,86 +35,143 @@ int currentModule = 0;
 
 int readInt(string token)
 {
-    return 0;
-}
-
-string checkSym(string token)
-{
-    return token;
-}
-
-string readIAER(string token)
-{
-    return token;
-}
-void passOne(vector<string> tokens)
-{
-    int i = 0;
-
-    while (i < tokens.size())
+    int out = 0;
+    try
     {
-        Module new_module;
-        if (moduleBaseTable.empty())
-        {
-            new_module.baseAddress = 0;
-        }
-        else
-        {
-            new_module.baseAddress = moduleBaseTable.back().baseAddress;
-        }
-
-        int defcount = readInt();
+        out = stoi(token);
     }
-}
-
-void passTwo(ifstream &inputFile)
-{
-}
-
-vector<string> getToken(string filename)
-{
-    vector<string> tokens;
-    ifstream inputFile(filename);
-    if (!inputFile.is_open())
+    catch (const std::invalid_argument &e)
     {
-        std::cerr << "Error: Unable to open file " << filename << std::endl;
+        cerr << "Invalid argument: " << e.what() << std::endl;
+    }
+    catch (const std::out_of_range &e)
+    {
+        cerr << "Out of range: " << e.what() << std::endl;
+    }
+    return out;
+}
+
+string readSym(string token)
+{
+    if (token.length() > 16)
+    {
+        cerr << "symbol name out of range";
+    }
+    return token;
+}
+
+char readIAER(string token)
+{
+    if (token.length() != 1)
+    {
+        cerr << "not a single char";
+    }
+    if (token[0] != 'm' && token[0] != 'a' && token[0] != 'r' && token[0] != 'i' && token[0] != 'e')
+    {
+        return token[0];
+    }
+    void passOne(vector<string> tokens)
+    {
+        int i = 0;
+
+        while (i < tokens.size())
+        {
+            Module new_module;
+            if (moduleBaseTable.empty())
+            {
+                new_module.baseAddress = 0;
+            }
+            else
+            {
+                new_module.baseAddress = moduleBaseTable.back().baseAddress;
+            }
+
+            int defcount = readInt(tokens[i]);
+            i += 1;
+            vector<Symbol> deflist;
+            for (int j = 0; j < defcount; j++)
+            {
+                Symbol curr;
+                curr.name = readSym(tokens[i]);
+                curr.address = readInt(tokens[i + 1]);
+                deflist.push_back(curr);
+                i += 2;
+            }
+            new_module.definitions = deflist;
+
+            int usecount = readInt(tokens[i]);
+            i += 1;
+            vector<string> uselist;
+            for (int j = 0; j < usecount; j++)
+            {
+                uselist.push_back(readSym(tokens[i]));
+                i += 1;
+            }
+            new_module.useList = uselist;
+
+            int instcount = readInt(tokens[i]);
+            i += 1;
+            vector<Instruction> instructions;
+            for (int j = 0; j < instcount; j++)
+            {
+                Instruction curr_ins;
+                curr_ins.addrMode = readIAER(tokens[i]);
+                curr_ins.operand = readInt(tokens[i + 1]);
+                i += 2;
+            }
+
+            moduleBaseTable.push_back(new_module);
+        }
+    }
+
+    void passTwo(ifstream & inputFile)
+    {
+    }
+
+    vector<string> getToken(string filename)
+    {
+        vector<string> tokens;
+        ifstream inputFile(filename);
+        if (!inputFile.is_open())
+        {
+            std::cerr << "Error: Unable to open file " << filename << std::endl;
+            return tokens;
+        }
+        string line;
+        string file_content;
+        while (getline(inputFile, line))
+        {
+            file_content += line;
+        }
+        inputFile.close();
+        regex delimiterPattern("[ \t\n]+");
+
+        sregex_token_iterator iter(file_content.begin(), file_content.end(), delimiterPattern, -1);
+        sregex_token_iterator end;
+
+        while (iter != end)
+        {
+            tokens.push_back(*iter);
+            ++iter;
+        }
         return tokens;
     }
-    string line;
-    string file_content;
-    while (getline(inputFile, line))
+
+    int main(int argc, char *argv[])
     {
-        file_content += line;
+        if (argc != 2)
+        {
+            cerr << "Usage: " << argv[0] << " <input_file>" << endl;
+            return 1;
+        }
+
+        vector<string> tokens = getToken(argv[1]);
+        // for (int i = 0; i < tokens.size(); i++)
+        // {
+        //     cout << tokens[i] << "\n";
+        // }
+        // cout << tokens.size();
+
+        // passOne(tokens);
+        return 0;
     }
-    inputFile.close();
-    regex delimiterPattern("[ \t\n]+");
-
-    sregex_token_iterator iter(file_content.begin(), file_content.end(), delimiterPattern, -1);
-    sregex_token_iterator end;
-
-    while (iter != end)
-    {
-        tokens.push_back(*iter);
-        ++iter;
-    }
-    return tokens;
-}
-
-int main(int argc, char *argv[])
-{
-    if (argc != 2)
-    {
-        cerr << "Usage: " << argv[0] << " <input_file>" << endl;
-        return 1;
-    }
-
-    vector<string> tokens = getToken(argv[1]);
-    // for (int i = 0; i < tokens.size(); i++)
-    // {
-    //     cout << tokens[i] << "\n";
-    // }
-    // cout << tokens.size();
-
-    // passOne(tokens);
-    return 0;
-}
