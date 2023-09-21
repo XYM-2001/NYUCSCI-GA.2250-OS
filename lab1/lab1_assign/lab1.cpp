03fdf3733870fb5032ee5c52b0cfa9fe7781c07f
Xinyu Meng <xmeng975@gmail.com>
2023-09-21 18:52:12 -0400

    revise

--- lab1/lab1_assign/lab1.cpp
+++ lab1/lab1_assign/lab1.cpp
@@ -18,7 +18,7 @@ struct Symbol
 struct Instruction
 {
     char addrMode;
-    int operand;
+    int instruction;
 };
 
 struct Module
@@ -57,6 +57,19 @@ string readSym(string token)
     {
         cerr << "symbol name out of range";
     }
+    if (!isalpha(token[0]))
+    {
+        cerr << "symbol name not starting with an Alpha char";
+    }
+    if (token.length() > 1)
+    {
+        regex pattern("^[a-zA-Z]+$|^[0-9]+$");
+        if (!regex_match(token.substr(1), pattern))
+        {
+            cerr << "symbol name wrong format";
+        }
+    }
+
     return token;
 }
 
@@ -73,94 +86,142 @@ char readIAER(string token)
     return token[0];
 }
 
-void passOne(vector<string> tokens)
-{
-    int i = 0;
+// void passOne(ifstream &inputFile)
+// {
+//     int i = 0;
 
-    while (i < tokens.size())
-    {
-        Module new_module;
-        if (moduleBaseTable.empty())
-        {
-            new_module.baseAddress = 0;
-        }
-        else
-        {
-            new_module.baseAddress = moduleBaseTable.back().baseAddress + moduleBaseTable.back().instructions.size();
-        }
+//     while (i < tokens.size())
+//     {
+//         Module new_module;
+//         if (moduleBaseTable.empty())
+//         {
+//             new_module.baseAddress = 0;
+//         }
+//         else
+//         {
+//             new_module.baseAddress = moduleBaseTable.back().baseAddress + moduleBaseTable.back().instructions.size();
+//         }
 
-        int defcount = readInt(tokens[i]);
-        i += 1;
-        vector<Symbol> deflist;
-        for (int j = 0; j < defcount; j++)
-        {
-            Symbol curr;
-            curr.name = readSym(tokens[i]);
-            curr.address = readInt(tokens[i + 1]);
-            deflist.push_back(curr);
-            i += 2;
-        }
-        new_module.definitions = deflist;
+//         int defcount = readInt(tokens[i]);
+//         i += 1;
+//         vector<Symbol> deflist;
+//         for (int j = 0; j < defcount; j++)
+//         {
+//             Symbol curr;
+//             curr.name = readSym(tokens[i]);
+//             curr.address = readInt(tokens[i + 1]);
+//             deflist.push_back(curr);
+//             i += 2;
+//         }
+//         new_module.definitions = deflist;
 
-        int usecount = readInt(tokens[i]);
-        i += 1;
-        vector<string> uselist;
-        for (int j = 0; j < usecount; j++)
-        {
-            uselist.push_back(readSym(tokens[i]));
-            i += 1;
-        }
-        new_module.useList = uselist;
+//         int usecount = readInt(tokens[i]);
+//         i += 1;
+//         vector<string> uselist;
+//         for (int j = 0; j < usecount; j++)
+//         {
+//             uselist.push_back(readSym(tokens[i]));
+//             i += 1;
+//         }
+//         new_module.useList = uselist;
 
-        int instcount = readInt(tokens[i]);
-        i += 1;
-        vector<Instruction> instructions;
-        for (int j = 0; j < instcount; j++)
-        {
-            Instruction curr_ins;
-            curr_ins.addrMode = readIAER(tokens[i]);
-            curr_ins.operand = readInt(tokens[i + 1]);
-            instructions.push_back(curr_ins);
-            i += 2;
-        }
-        new_module.instructions = instructions;
-        moduleBaseTable.push_back(new_module);
-    }
-}
+//         int instcount = readInt(tokens[i]);
+//         i += 1;
+//         vector<Instruction> instructions;
+//         for (int j = 0; j < instcount; j++)
+//         {
+//             Instruction curr_ins;
+//             curr_ins.addrMode = readIAER(tokens[i]);
+//             curr_ins.instruction = readInt(tokens[i + 1]);
+//             if ((curr_ins.instruction / 1000) >= 10)
+//             {
+//                 cerr << "instruction opecode >= 10";
+//             }
+//             if (curr_ins.addrMode == 'A')
+//             {
+//                 if ((curr_ins.instruction % 1000) >= 512)
+//                 {
+//                     cerr << "instruction operand greater than machine size";
+//                 }
+//             }
+//             else if (curr_ins.addrMode == 'I')
+//             {
+//                 if ((curr_ins.instruction % 1000) >= 900)
+//                 {
+//                     cerr << "instruction operand greater than 900";
+//                 }
+//             }
+//             instructions.push_back(curr_ins);
+//             i += 2;
+//         }
+//         new_module.instructions = instructions;
+//         moduleBaseTable.push_back(new_module);
+//     }
+// }
 
 void passTwo(ifstream &inputFile)
 {
 }
 
-vector<string> getToken(string filename)
+// vector<string> getToken(string filename)
+// {
+//     vector<string> tokens;
+//     ifstream inputFile(filename);
+//     if (!inputFile.is_open())
+//     {
+//         std::cerr << "Error: Unable to open file " << filename << std::endl;
+//         return tokens;
+//     }
+//     std::string file_content((std::istreambuf_iterator<char>(inputFile)),
+//                              (std::istreambuf_iterator<char>()));
+//     inputFile.close();
+//     regex delimiterPattern("[ \t\n]+");
+
+//     sregex_token_iterator iter(file_content.begin(), file_content.end(), delimiterPattern, -1);
+//     sregex_token_iterator end;
+
+//     while (iter != end)
+//     {
+//         tokens.push_back(*iter);
+//         ++iter;
+//     }
+//     return tokens;
+// }
+
+string getToken(ifstream &inputFile)
 {
-    vector<string> tokens;
-    ifstream inputFile(filename);
-    if (!inputFile.is_open())
-    {
-        std::cerr << "Error: Unable to open file " << filename << std::endl;
-        return tokens;
-    }
-    // string line;
-    // string file_content;
-    // while (getline(inputFile, line))
-    // {
-    //     file_content += line;
-    // }
-    std::string file_content((std::istreambuf_iterator<char>(inputFile)),
-                             (std::istreambuf_iterator<char>()));
-    inputFile.close();
-    regex delimiterPattern("[ \t\n]+");
+    std::string token;
 
-    sregex_token_iterator iter(file_content.begin(), file_content.end(), delimiterPattern, -1);
-    sregex_token_iterator end;
-
-    while (iter != end)
+    // Skip leading whitespace characters
+    while (inputFile >> std::ws)
     {
-        tokens.push_back(*iter);
-        ++iter;
+        char c = inputFile.get();
+        if (c == ' ' || c == '\t' || c == '\n')
+        {
+            continue; // Skip delimiter characters
+        }
+        else
+        {
+            token += c; // Append the character to the token
+            break;
+        }
     }
-    return tokens;
+
+    // Read characters until a delimiter or end of file is encountered
+    while (true)
+    {
+        char c = inputFile.get();
+        if (inputFile.eof() || c == ' ' || c == '\t' || c == '\n')
+        {
+            break; // Return the token if a delimiter or end of file is reached
+        }
+        else
+        {
+            token += c; // Append the character to the token
+        }
+    }
+
+    return token;
 }
 
 void printModule(const Module &module)
@@ -182,7 +243,7 @@ void printModule(const Module &module)
     std::cout << "Instructions:" << std::endl;
     for (const Instruction &instruction : module.instructions)
     {
-        std::cout << "Address Mode: " << instruction.addrMode << ", Operand: " << instruction.operand << std::endl;
+        std::cout << "Address Mode: " << instruction.addrMode << ", Operand: " << instruction.instruction << std::endl;
     }
 }
 
@@ -194,16 +255,25 @@ int main(int argc, char *argv[])
         return 1;
     }
 
-    vector<string> tokens = getToken(argv[1]);
+    ifstream inputFile(argv[1]);
+    if (!inputFile.is_open())
+    {
+        cerr << "Error: Unable to open file " << argv[1] << endl;
+    }
+    // vector<string> tokens = getToken(argv[1]);
+
     // for (int i = 0; i < tokens.size(); i++)
     // {
     //     cout << tokens[i] << "\n";
     // }
     // cout << tokens.size();
-    passOne(tokens);
-    for (Module i : moduleBaseTable)
-    {
-        printModule(i);
-    }
+
+    // passOne(inputFile);
+
+    // for (Module i : moduleBaseTable)
+    // {
+    //     printModule(i);
+    // }
+    // cout << moduleBaseTable.size();
     return 0;
 }
