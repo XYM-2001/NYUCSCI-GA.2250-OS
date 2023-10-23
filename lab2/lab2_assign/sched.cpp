f7cf68f93f3e8dc3568e3d90472a78de46aadbf7
Xinyu Meng <xmeng975@gmail.com>
2023-10-23 19:39:27 -0400

    updated F L S scheduler

--- lab2/lab2_assign/sched.cpp
+++ lab2/lab2_assign/sched.cpp
@@ -5,15 +5,24 @@
 #include <cstdlib>
 #include <cstring>
 #include <getopt.h>
+#include <sstream>
+#include <string>
+#include <list>
+#include <map>
+#include <iomanip>
 
 using namespace std;
 
-int ofs = 0;
+vector<int> randvals;
+int ofs = 1;
+int maxprio = 4;
+int CURRENT_TIME = 0;
+int TOTAL_IO;
+bool CALL_SCHEDULER = false;
+int block_num = 0;
+int totalb = 0;
+int block_time = 0;
 
-int myrandom(int burst)
-{
-    return 1 + (randvals[ofs] % burst);
-}
 // Define process states using an enumeration
 enum ProcessState
 {
@@ -24,18 +33,53 @@ enum ProcessState
     TERMINATED
 };
 
+map<ProcessState, string> stateToString = {
+    {CREATED, "CREATED"},
+    {READY, "READY"},
+    {RUNNING, "RUNNING"},
+    {BLOCKED, "BLOCKED"},
+    {TERMINATED, "DONE"}};
+
+enum Transition
+{
+    TRANS_TO_READY,
+    TRANS_TO_PREEMPT,
+    TRANS_TO_RUN,
+    TRANS_TO_BLOCK,
+    DONE
+};
+
+int myrandom(int burst)
+{
+    int result;
+    if (ofs > randvals[0])
+    {
+        ofs = 1;
+    }
+    result = 1 + (randvals[ofs] % burst);
+    ofs++;
+    return result;
+}
+
 // Define a Process class to represent processes
 class Process
 {
 public:
-    int pid;            // Process ID
-    int at;             // Arrival Time
-    int tc;             // Total CPU Time
-    int cb;             // CPU Burst
-    int io;             // IO Burst
-    int static_prio;    // Static Priority
-    int dynamic_prio;   // Dynamic Priority
-    int state_ts;       // State Transition Timestamp
+    int pid;          // Process ID
+    int at;           // Arrival Time
+    int tc;           // Total CPU Time
+    int cb;           // CPU Burst
+    int io;           // IO Burst
+    int static_prio;  // Static Priority
+    int dynamic_prio; // Dynamic Priority
+    int cpu_burst;    // cpu burst
+    int io_burst;     // io burst
+    int state_ts;
+    int rem;
+    int ft;
+    int ready;
+    int it = 0;
+    int cw = 0;
     ProcessState state; // Current State
 
     // Constructor to initialize a process
@@ -48,7 +92,7 @@ public:
         this->io = io;
         this->static_prio = myrandom(maxprio);
         this->dynamic_prio = this->static_prio - 1;
-        this->state_ts = at;
+        this->rem = tc;
         this->state = CREATED;
     }
 
@@ -59,63 +103,361 @@ public:
 class Event
 {
 public:
-    int timestamp;    // Event Timestamp
-    Process *process; // Associated Process
-    int transition;   // State Transition
+    int timestamp;         // Event Timestamp
+    Process *process;      // Associated Process
+    Transition transition; // State Transition
+    int burst;
 
     // Constructor to initialize an event
-    Event(int timestamp, Process *process, int transition)
+    Event(int timestamp, Process *process, Transition transition)
     {
         this->timestamp = timestamp;
         this->process = process;
         this->transition = transition;
+        this->burst = 0;
     }
 };
 
 class Scheduler
 {
 public:
-    virtual void addProcess(Process *process) = 0;
     virtual Process *getNextProcess() = 0;
-    virtual bool isReadyQueueEmpty() const = 0;
-    virtual ~Scheduler() {} // Virtual destructor for proper cleanup
+    virtual void addrunQ(Process *process) = 0;
+    virtual string toString() = 0;
+    virtual int getQuantum() = 0;
 };
 
 class FCFS_Scheduler : public Scheduler
 {
 public:
-    FCFS_Scheduler()
+    string toString() override
     {
-        // Initialize FCFS scheduler-specific data if needed
+        return "FCFS";
     }
 
     // Implement the interface functions
-    void addProcess(Process *process) override
+    void addrunQ(Process *process) override
     {
-        readyQueue.push(process);
+        runQueue.push_back(process);
     }
 
     Process *getNextProcess() override
     {
-        if (!readyQueue.empty())
+        if (!runQueue.empty())
         {
-            Process *nextProcess = readyQueue.front();
-            readyQueue.pop();
+            Process *nextProcess = runQueue.front();
+            runQueue.pop_front();
             return nextProcess;
         }
         return nullptr;
     }
 
-    bool isReadyQueueEmpty() const override
+    int getQuantum() override
     {
-        return readyQueue.empty();
+        return 0;
     }
 
 private:
-    std::queue<Process *> readyQueue;
+    list<Process *> runQueue;
 };
 
-// Define a Scheduler interface and its derived classes (e.g., FCFS, RR, PRIO, SRTF)
+class LCFS_Scheduler : public Scheduler
+{
+public:
+    string toString()
+    {
+        return "LCFS";
+    }
+
+    // Implement the interface functions
+    void addrunQ(Process *process) override
+    {
+        runQueue.push_front(process);
+    }
+
+    Process *getNextProcess() override
+    {
+        if (!runQueue.empty())
+        {
+            Process *nextProcess = runQueue.front();
+            runQueue.pop_front();
+            return nextProcess;
+        }
+        return nullptr;
+    }
+
+    int getQuantum() override
+    {
+        return 0;
+    }
+
+private:
+    list<Process *> runQueue;
+};
+
+class SRTF_Scheduler : public Scheduler
+{
+public:
+    string toString() override
+    {
+        return "SRTF";
+    }
+
+    // Implement the interface functions
+    void addrunQ(Process *process) override
+    {
+        if (runQueue.empty())
+        {
+            runQueue.push_back(process);
+        }
+        else
+        {
+            // Find the correct position to insert the event.
+            auto it = runQueue.begin();
+            while (it != runQueue.end() && ((*it)->rem < process->rem))
+            {
+                ++it;
+            }
+
+            // If there are events with the same timestamp, insert the new event after them.
+            while (it != runQueue.end() && (*it)->rem == process->rem)
+            {
+                ++it;
+            }
+
+            runQueue.insert(it, process);
+        }
+    }
+
+    Process *getNextProcess() override
+    {
+        if (!runQueue.empty())
+        {
+            Process *nextProcess = runQueue.front();
+            runQueue.pop_front();
+            return nextProcess;
+        }
+        return nullptr;
+    }
+
+    int getQuantum() override
+    {
+        return 0;
+    }
+
+private:
+    list<Process *> runQueue;
+};
+
+class RR_Scheduler : public Scheduler
+{
+public:
+    RR_Scheduler(int quantum)
+    {
+        this->quantum = quantum;
+    }
+    string toString() override
+    {
+        return "RR" + quantum;
+    }
+
+    // Implement the interface functions
+    void addrunQ(Process *process) override
+    {
+        runQueue.push_back(process);
+    }
+
+    Process *getNextProcess() override
+    {
+        if (!runQueue.empty())
+        {
+            Process *nextProcess = runQueue.front();
+            runQueue.pop_front();
+            return nextProcess;
+        }
+        return nullptr;
+    }
+
+    int getQuantum() override
+    {
+        return quantum;
+    }
+
+private:
+    list<Process *> runQueue;
+    int quantum;
+};
+
+Process *CURRENT_RUNNING_PROCESS = nullptr;
+Scheduler *scheduler;
+list<Event *> eventQueue;
+queue<Process *> res;
+
+void add_event(Event *evt)
+{
+    if (eventQueue.empty())
+    {
+        eventQueue.push_back(evt);
+    }
+    else
+    {
+        // Find the correct position to insert the event.
+        auto it = eventQueue.begin();
+        while (it != eventQueue.end() && ((*it)->timestamp < evt->timestamp))
+        {
+            ++it;
+        }
+
+        // If there are events with the same timestamp, insert the new event after them.
+        while (it != eventQueue.end() && (*it)->timestamp == evt->timestamp)
+        {
+            ++it;
+        }
+
+        eventQueue.insert(it, evt);
+    }
+}
+
+Event *get_event()
+{
+    if (!eventQueue.empty())
+    {
+        Event *event = eventQueue.front();
+        eventQueue.pop_front();
+        return event;
+    }
+    return nullptr;
+}
+
+int get_next_event_time()
+{
+    if (!eventQueue.empty())
+    {
+        // The eventQueue is not empty, so you can access the timestamp of the first event.
+        return eventQueue.front()->timestamp;
+    }
+    else
+    {
+        // Handle the case when the eventQueue is empty, e.g., return a special value.
+        return -1; // You can choose an appropriate value or error code.
+    }
+}
+
+void Simulation()
+{
+    Event *evt;
+    while ((evt = get_event()))
+    {
+        Process *proc = evt->process;
+        CURRENT_TIME = evt->timestamp;
+        Transition transition = evt->transition;
+        int timeInPrevState = CURRENT_TIME - (proc->io_burst + proc->cpu_burst);
+        delete evt;
+        evt = nullptr;
+        cout << CURRENT_TIME << " " << proc->pid << " " << proc->io_burst + proc->cpu_burst << ": ";
+        switch (transition)
+        {
+        case TRANS_TO_READY:
+            // must come from BLOCKED or CREATED
+            // add to run queue, no event created
+            cout << stateToString[proc->state] << " -> READY";
+            scheduler->addrunQ(proc);
+            if (proc->state == BLOCKED)
+            {
+                proc->io_burst = 0;
+                block_num -= 1;
+                if (block_num == 0)
+                {
+                    totalb += CURRENT_TIME - block_time;
+                }
+            }
+            if (proc->state == RUNNING)
+            {
+            }
+            proc->state = READY;
+            proc->ready = CURRENT_TIME;
+            CALL_SCHEDULER = true;
+            break;
+        case TRANS_TO_PREEMPT:
+            // must come from RUNNING (preemption)
+            // add to runqueue (no event is generated)
+            CALL_SCHEDULER = true;
+            break;
+        case TRANS_TO_RUN:
+            // create event for either preemption or blocking
+            proc->cpu_burst = myrandom(proc->cb);
+            if ((proc->rem - proc->cpu_burst) <= 0)
+            {
+                proc->cpu_burst = proc->rem;
+
+                cout << stateToString[proc->state] << " -> RUNNG cb=" << proc->cpu_burst << " rem=" << proc->rem << " prio=" << proc->dynamic_prio;
+                evt = new Event(CURRENT_TIME + proc->cpu_burst, proc, DONE);
+            }
+            else
+            {
+                cout << stateToString[proc->state] << " -> RUNNG cb=" << proc->cpu_burst << " rem=" << proc->rem << " prio=" << proc->dynamic_prio;
+                evt = new Event(CURRENT_TIME + proc->cpu_burst, proc, TRANS_TO_BLOCK);
+            }
+            if (proc->state == READY)
+            {
+                proc->cw += CURRENT_TIME - proc->ready;
+            }
+            proc->state = RUNNING;
+            add_event(evt);
+            break;
+        case TRANS_TO_BLOCK:
+            // create an event for when process becomes READY again
+            proc->rem -= proc->io_burst;
+            proc->io_burst = myrandom(proc->io);
+            cout << stateToString[proc->state] << " -> BLOCK ib=" << proc->io_burst << " rem=" << proc->rem;
+            proc->it += proc->io_burst;
+            if (proc->state == READY)
+            {
+                proc->cw += proc->ready - CURRENT_TIME;
+            }
+            if (block_num == 0)
+            {
+                block_time = CURRENT_TIME;
+            }
+            block_num += 1;
+            proc->state = BLOCKED;
+            CURRENT_RUNNING_PROCESS = nullptr;
+            evt = new Event(CURRENT_TIME + proc->io_burst, proc, TRANS_TO_READY);
+            add_event(evt);
+            CALL_SCHEDULER = true;
+            break;
+        case DONE:
+            proc->rem -= proc->cpu_burst;
+            proc->ft = CURRENT_TIME;
+            CURRENT_RUNNING_PROCESS = nullptr;
+            cout << "Done";
+            CALL_SCHEDULER = true;
+            break;
+        }
+        if (CALL_SCHEDULER)
+        {
+            if (get_next_event_time() == CURRENT_TIME)
+            {
+                cout << endl;
+                continue; // process next event from Event queue
+            }
+            CALL_SCHEDULER = false;
+            if (CURRENT_RUNNING_PROCESS == nullptr)
+            {
+                CURRENT_RUNNING_PROCESS = scheduler->getNextProcess();
+                if (CURRENT_RUNNING_PROCESS == nullptr)
+                {
+                    cout << endl;
+                    continue;
+                }
+                // create event to make this process runnable for same time.
+                evt = new Event(CURRENT_TIME, CURRENT_RUNNING_PROCESS, TRANS_TO_RUN);
+                add_event(evt);
+            }
+        }
+        cout << endl;
+    }
+}
 
 int main(int argc, char *argv[])
 {
@@ -168,23 +510,105 @@ int main(int argc, char *argv[])
         std::cerr << "Error: Missing input file and/or rand file." << std::endl;
         return 1; // Return an error code for missing input files
     }
-    // Now you can use the parsed options and arguments
-    // For example, print them to check the values:
-    std::cout << "Verbose: " << verbose << std::endl;
-    std::cout << "Trace: " << trace << std::endl;
-    std::cout << "Error: " << error << std::endl;
-    std::cout << "Process: " << process << std::endl;
-    std::cout << "Scheduler Spec: " << schedspec << std::endl;
-    std::cout << "Input File: " << inputfile << std::endl;
-    std::cout << "Rand File: " << randfile << std::endl;
 
+    // // std::cout << "Verbose: " << verbose << std::endl;
+    // // std::cout << "Trace: " << trace << std::endl;
+    // // std::cout << "Error: " << error << std::endl;
+    // // std::cout << "Process: " << process << std::endl;
+    // // std::cout << "Scheduler Spec: " << schedspec << std::endl;
+    // // std::cout << "Input File: " << inputfile << std::endl;
+    // // std::cout << "Rand File: " << randfile << std::endl;
+
+    ifstream randFile(randfile);
+    int num;
+    if (!(randFile >> num))
+    {
+        cerr << "cannot read size from randfile" << endl;
+        exit(1);
+    }
+    randvals.push_back(num);
+    for (int i = 0; i < randvals[0]; i++)
+    {
+        randFile >> num;
+        randvals.push_back(num);
+    }
+    randFile.close();
     // Initialize the event queue and other data structures
-
+    if (schedspec[0] == 'F')
+    {
+        scheduler = new FCFS_Scheduler();
+    }
+    else if (schedspec[0] == 'L')
+    {
+        scheduler = new LCFS_Scheduler();
+    }
+    else if (schedspec[0] == 'S')
+    {
+        scheduler = new SRTF_Scheduler();
+    }
+    else if (schedspec[0] == 'R')
+    {
+        scheduler = new RR_Scheduler(stoi(schedspec.substr(1)));
+    }
+    else if (schedspec[0] == 'P')
+    {
+    }
+    else if (schedspec[0] == 'E')
+    {
+    }
+    else
+    {
+        cerr << "error in scheduler speficication" << endl;
+        exit(1);
+    }
     // Read input processes and create initial events
-
+    ifstream inputFile(inputfile);
+    int pid = 0;
+    string line;
+    while (getline(inputFile, line))
+    {
+        istringstream iss(line);
+        string token;
+        iss >> token;
+        int at = stoi(token); // Arrival Time
+        iss >> token;
+        int tc = stoi(token); // Total CPU Time
+        iss >> token;
+        int cb = stoi(token); // CPU Burst
+        iss >> token;
+        int io = stoi(token); // IO Burst
+        Process *process = new Process(pid, at, tc, cb, io);
+        pid++;
+        res.push(process);
+        Event *evt = new Event(at, process, Transition(TRANS_TO_READY));
+        add_event(evt);
+    }
+    inputFile.close();
     // Run the simulation
-
+    Simulation();
     // Calculate and print simulation results
-
+    cout << scheduler->toString() << endl;
+    int total_cpu = 0;
+    int total_turnaround = 0;
+    int proclen = res.size();
+    int total_cw = 0;
+    while (!res.empty())
+    {
+        Process *proc = res.front();
+        total_cpu += proc->tc;
+        total_turnaround += proc->ft - proc->at;
+        total_cw += proc->cw;
+        cout << setw(4) << setfill('0') << proc->pid << ": ";
+        cout << setw(5) << right << setfill(' ') << proc->at << " " << proc->tc << " " << proc->cb << " " << proc->io << " " << proc->static_prio << " | " << proc->ft
+             << " " << proc->ft - proc->at << " " << proc->it << " " << proc->cw << endl;
+        delete proc;
+        res.pop();
+    }
+    cout << "SUM: " << CURRENT_TIME << " " << fixed << setprecision(2)
+         << 100 * (static_cast<double>(total_cpu) / CURRENT_TIME) << " "
+         << 100 * (static_cast<double>(totalb) / CURRENT_TIME) << " "
+         << static_cast<double>(total_turnaround) / proclen << " "
+         << static_cast<double>(total_cw) / proclen << " " << setprecision(3)
+         << 100 * static_cast<double>(proclen) / CURRENT_TIME << endl;
     return 0;
 }
